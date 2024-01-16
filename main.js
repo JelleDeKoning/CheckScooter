46de61c3f890b34cbb559c1cc9c4e06ca7f5f600
Jelle <54027479+JelleDeKoning@users.noreply.github.com>
2024-01-16 12:25:23 +0100

    1

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ scene.add(ambientLight);
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+camera.position.y = 5;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -21,7 +22,11 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.screenSpacePanning = false;
-controls.maxPolarAngle = Math.PI / 2;
+
+
+// Disable rotation controls
+controls.enableZoom = false;
+
 
 // Load GLTF Model
 let model;
@@ -30,20 +35,14 @@ const loader = new GLTFLoader();
 
 loader.load('/scene.gltf', function (gltf) {
     model = gltf.scene;
+
+    // Set initial position and scale
+    model.position.y = 3
+    model.scale.set(0.5, 0.5, 0.5); // Change the values as needed for your scene
+
     scene.add(model);
 }, undefined, function (error) {
     console.error(error);
-});
-
-// Handle Window Resize
-window.addEventListener('resize', () => {
-    const newWidth = window.innerWidth;
-    const newHeight = window.innerHeight;
-
-    camera.aspect = newWidth / newHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(newWidth, newHeight);
 });
 
 // Animation Loop
@@ -60,3 +59,31 @@ function animate() {
 }
 
 animate();
+
+
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
